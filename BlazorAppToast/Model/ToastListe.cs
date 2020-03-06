bc961505ff757378a55ab44b2bacaf4bc4eacf02
Hannes Preishuber <hannesp@ppedv.de>
2020-03-06 08:24:13 +0100

    Projektdateien hinzufügen.

---
+++ BlazorAppToast/Model/ToastListe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppToast.Model
+{
+    public class ToastListe
+         {
+             public event Action OnToastsUpdated;
+             public ToastListe()
+             {
+                 Toasts = new List<ToastItem>();
+              }
+             public void Add(string par1)
+             {
+               var t = new ToastItem();
+               t.Titel = par1;
+               Toasts.Add(t);
+     
+                OnToastsUpdated?.Invoke();
+            }
+            public void Remove(string par1)
+            {
+                var item = Toasts.Where(x => x.Titel == par1).FirstOrDefault();
+                Toasts.Remove(item);
+     
+                OnToastsUpdated?.Invoke();
+            }
+     
+            public List<ToastItem> Toasts { get; set; }
+        }
+}
